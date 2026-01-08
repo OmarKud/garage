abf3f65ca3bdef6c95180eb1ce6aa368340f39d2
Mohammad-KhairBaalbaki <mohammad.k.baalbaki@gmail.com>
2026-01-08 12:05:57 +0300

    files done

--- MyOpenGLLegacy/src/main.cpp
+++ MyOpenGLLegacy/src/main.cpp
@@ -4,12 +4,12 @@
 
 int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, PWSTR, int)
 {
-    //mohammad
+    
     App app;
     if (!app.Init(hInst))
         return 0;
-    //0
+    
     return app.Run();
 
-    //finally
+    
 }
